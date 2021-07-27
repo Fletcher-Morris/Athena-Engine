7e6ba2030b48351b3f0de2cb70f9fddb0874cdeb
Fletcher Morris <odmoandtheboy@gmail.com>
2021-07-27 19:26:52 +0100

    Added premake files

---
+++ premake5.lua
@@ -0,0 +1,110 @@
+workspace "Athena"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release"
+	}
+
+OutputDir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.archetecture}"
+
+project "Athena"
+	location "Athena"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. OutputDir .. "%{prj.name}")
+	objdir ("bin-int/" .. OutputDir .. "%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.hpp",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	libdirs
+	{
+	}
+
+	links
+	{
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"ATH_PLAT_WIN",
+			"ATH_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. OutputDir .. "/Forge")
+		}
+
+	filter "configurations:Debug"
+		defines "ATH_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "ATH_RELEASE"
+		optimize "On"
+
+
+
+
+		
+project "Forge"
+	location "Forge"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. OutputDir .. "%{prj.name}")
+	objdir ("bin-int/" .. OutputDir .. "%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.hpp",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Athena/vendor/spdlog/include",
+		"Athena/src"
+	}
+
+	links
+	{
+		"Athena"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"ATH_PLAT_WIN"
+		}
+
+	filter "configurations:Debug"
+		defines "ATH_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "ATH_RELEASE"
+		optimize "On"
