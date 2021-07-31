6ae4619d53e36e30618941077b48754cd4279f22
Fletcher Morris <odmoandtheboy@gmail.com>
2021-07-31 01:26:55 +0100

    More work on vulkcan

--- Athena/src/Athena/App.cpp
+++ Athena/src/Athena/App.cpp
@@ -11,6 +11,7 @@ namespace Athena
 		m_appWindow = std::unique_ptr<Window>(Window::Create());
 		CreateVulkanInstance();
 		ATH_ENGINE_INFO("Created Vulkan instance!");
+		SelectVulkanDevice();
 	}
 
 	Athena::App::~App()
@@ -72,7 +73,7 @@ namespace Athena
 	{
 		m_vulkanDevice = VK_NULL_HANDLE;
 		uint32_t vkDeviceCount = 0;
-		vkEnumeratePhysicalDevices(m_vulkanInstance, &vkDeviceCount, nullptr);
+		VkResult enumerateDevicesResult = vkEnumeratePhysicalDevices(m_vulkanInstance, &vkDeviceCount, nullptr);
 		ATH_ENGINE_ASSERT(vkDeviceCount == 0, "Failed to find GPU with Vulkan support!");
 
 		std::vector<VkPhysicalDevice> foundVkDevices(vkDeviceCount);
@@ -88,10 +89,34 @@ namespace Athena
 		ATH_ENGINE_ASSERT(gpuSuitable, "Failed to find a suitable GPU!");
 		if (gpuSuitable == false) return false;
 
-		return false;
+		return true;
 	}
 	bool App::IsVulkanDeviceSuitable(VkPhysicalDevice device)
 	{
+		return true;
+
+		VkPhysicalDeviceProperties deviceProperties;
+		VkPhysicalDeviceFeatures deviceFeatures;
+		vkGetPhysicalDeviceProperties(device, &deviceProperties);
+		vkGetPhysicalDeviceFeatures(device, &deviceFeatures);
+		std::string deviceName = deviceProperties.deviceName;
+
+		bool isDiscreteGpu = deviceProperties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU;
+		if (isDiscreteGpu == false)
+		{
+			ATH_ENGINE_WARN("GPU device '{0}' is not discrete!", deviceName);
+			return false;
+		}
+
+		bool hasGeomShader = deviceFeatures.geometryShader;
+		if (hasGeomShader == false)
+		{
+			ATH_ENGINE_WARN("GPU device '{0}' does not support geometry shaders!", deviceName);
+			return false;
+		}
+
+		ATH_ENGINE_INFO("GPU device '{0}' is suitable!", deviceName);
+
 		return true;
 	}
 }
